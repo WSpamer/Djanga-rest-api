0a9177f579e4e7bb0f67ffe89a68fabe33a5668f
Wian Spamer <spamer24310@gmail.com>
2021-06-04 17:48:20 +0200

    Changed Views returns

--- manageProjects/views.py
+++ manageProjects/views.py
@@ -6,6 +6,10 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
 # Create your views here.
 
 
@@ -15,14 +19,14 @@ def project_list(request):
     if request.method == 'GET':
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
-        return Response(serializer.data)
+        return JsonResponse(serializer.data, safe=False)
 
     elif request.method == 'POST':
         serializer = ProjectSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
